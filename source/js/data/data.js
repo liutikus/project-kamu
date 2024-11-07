3f0c985ffc7379e902104134a7fb226d69011d5e
liutikus <niclcodreanu@gmail.com>
2024-11-07 15:35:25 +0200

    kamuv2

--- source/js/data/data.js
+++ source/js/data/data.js
@@ -36,3 +36,139 @@ export function getGenres(data){
         .catch(err => console.error(err));
 
 }
+
+export function renderMovies(){
+    const options = {
+        method: 'GET',
+        headers: {
+          accept: 'application/json',
+          Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3OTZhMTdhODM0YmZmZjI1ZmM5ZjgxMGNiYzM0MmZhNCIsIm5iZiI6MTcyODU0NjQ1Ny4wNTA3NTksInN1YiI6IjY3MDc4MzcwZDA2MTZjN2IxOWZiNDEzNyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.uiEdUQrKbS11i0JkXLX9TB5yWoAFWJ16YoNjysd_LCA'
+        }
+      };
+      
+      fetch(`https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1`, options)
+        .then(response => response.json())
+        .then(response => {
+            const data=response
+            console.log(data)
+            renderContent(data, 'movies')
+            getGenres(data)
+            createCarousell('movies')
+            showInfo(data)
+            
+        })
+        .catch(err => console.error(err));
+    
+    }
+
+function createCarousell(contentType){
+    var elem = document.querySelector(`.main-carousel-${contentType}`);
+    var flkty = new Flickity( elem, {
+        // options
+        cellAlign: 'left',
+        contain: true ,
+        groupCells: true
+    });
+
+}
+
+
+  function showInfo(data){
+     
+    mouseOver(data)
+    mouseOut(data)
+
+  }  
+  
+function mouseOver(data){
+    document.querySelectorAll('.poster-carousell')
+    .forEach((info) => {
+      info.addEventListener('mouseover', ()=>{
+          const movieId= info.dataset.movieId
+          const moviesData= data.results
+
+          moviesData.forEach((movie)=>{
+
+              if(Number(movieId) === movie.id){
+                  document.querySelector(`.js-movie-${movie.id}`)
+                      .classList.remove('movie-info-off')
+                 
+              }
+          })
+      })
+    });
+}
+  
+function mouseOut(data){
+    document.querySelectorAll('.poster-carousell')
+    .forEach((info) => {
+      info.addEventListener('mouseout', ()=>{
+          const movieId= info.dataset.movieId
+          const moviesData= data.results
+
+          moviesData.forEach((movie)=>{
+
+              if(Number(movieId) === movie.id){
+                  document.querySelector(`.js-movie-${movie.id}`)
+                      .classList.add('movie-info-off')
+                 
+              }
+          })
+      })
+    });
+}
+
+export function renderSeries(){
+    const options = {
+        method: 'GET',
+        headers: {
+          accept: 'application/json',
+          Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3OTZhMTdhODM0YmZmZjI1ZmM5ZjgxMGNiYzM0MmZhNCIsIm5iZiI6MTcyODU0NjQ1Ny4wNTA3NTksInN1YiI6IjY3MDc4MzcwZDA2MTZjN2IxOWZiNDEzNyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.uiEdUQrKbS11i0JkXLX9TB5yWoAFWJ16YoNjysd_LCA'
+        }
+      };
+      
+      fetch('https://api.themoviedb.org/3/tv/top_rated?language=en-US&page=1', options)
+        .then(res => res.json())
+        .then(res =>{
+            const data = res
+            console.log(data)
+            renderContent(data, 'series' )
+            getGenres(data)
+            createCarousell('series')
+            showInfo(data)
+        })
+        .catch(err => console.error(err));
+}
+
+function renderContent(data, contentType){
+    
+    let html=``;
+    const movieData=data.results
+    
+    movieData.forEach(movie=> {
+        let contentTitle
+        if(movie.title){
+            contentTitle= movie.title
+
+        }
+        else{
+            contentTitle= movie.name
+        }
+        html+=`
+                <div class="poster-carousell" data-movie-id="${movie.id}" >
+                    <img class="movie-poster" src="https://image.tmdb.org/t/p/original/${movie.poster_path}" class="card-img-top" alt="...">
+                    <div class="movie-info movie-info-off js-movie-${movie.id}" >
+                        <p class="movie-title">${contentTitle} <span style="display:block;">(${formatDate(movie.release_date)})</span></p>
+                        <p class="movie-rating">★ ${movie.vote_average} (${movie.vote_count})</p>
+                        <a href="#" class="wishlist-btn">Wishlist</a>
+                    </div>
+                </div>
+              `
+        document.querySelector(`.${contentType}-container`)
+            .innerHTML=html
+        
+            
+        });
+    }
+   
+
--- source/js/index.js
+++ source/js/index.js
@@ -1,73 +1,8 @@
-import { formatDate,getGenres } from "./data/data.js";
+import { renderMovies, renderSeries } from "./data/data.js";
 
-let data=[]
-let number= 1
 
-renderPage()
-
-function renderPage(){
-const options = {
-    method: 'GET',
-    headers: {
-      accept: 'application/json',
-      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3OTZhMTdhODM0YmZmZjI1ZmM5ZjgxMGNiYzM0MmZhNCIsIm5iZiI6MTcyODU0NjQ1Ny4wNTA3NTksInN1YiI6IjY3MDc4MzcwZDA2MTZjN2IxOWZiNDEzNyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.uiEdUQrKbS11i0JkXLX9TB5yWoAFWJ16YoNjysd_LCA'
-    }
-  };
-  
-  fetch(`https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=${number}`, options)
-    .then(response => response.json())
-    .then(response => {
-        const data=response
-        console.log(data)
-        renderMovies(data)
-        getGenres(data)
-        createCarousell()
-        showInfo(data)
-        
-    })
-    .catch(err => console.error(err));
-
-function renderMovies(data){
-
-    let html=``;
-    const movieData=data.results
-    movieData.forEach(movie=> {
-
-        html+=`
-                <div class="poster-carousell" data-movie-id="${movie.id}" >
-                    <img class="movie-poster" src="https://image.tmdb.org/t/p/original/${movie.poster_path}" class="card-img-top" alt="...">
-                    <div class="movie-info movie-info-off js-movie-${movie.id}" >
-                        <p class="movie-title">${movie.title} <span>(${formatDate(movie.release_date)})</span></p>
-                        <p class="movie-rating">★ ${movie.vote_average} (${movie.vote_count})</p>
-                        <a href="#" class="wishlist-btn">Wishlist</a>
-                    </div>
-                </div>
-              `
-        document.querySelector('.movies-container')
-            .innerHTML=html
-        
-            
-        });
-    }
-   
-}
-
-document.querySelector('.js-next-page-btn')
-    .addEventListener('click', ()=>{
-       number++
-       renderPage()
-    })
-
-document.querySelector('.js-previous-page-btn')
-    .addEventListener('click', ()=>{
-        if(number <=1){
-            return
-        }
-        else{
-            number--
-            renderPage()
-        }
-    })
+renderMovies()
+renderSeries()
 
 document.querySelector('.js-burger-container')
     .addEventListener('click', () =>{
@@ -80,60 +15,5 @@ document.querySelector('.js-burger-container')
     })
 
 
-function createCarousell(){
-    var elem = document.querySelector('.main-carousel');
-    var flkty = new Flickity( elem, {
-        // options
-        cellAlign: 'left',
-        contain: true ,
-        groupCells: true
-    });
 
-}
-
-
-  function showInfo(data){
-     
-    mouseOver(data)
-    mouseOut(data)
-
-  }  
-  
-function mouseOver(data){
-    document.querySelectorAll('.poster-carousell')
-    .forEach((info) => {
-      info.addEventListener('mouseover', ()=>{
-          const movieId= info.dataset.movieId
-          const moviesData= data.results
-
-          moviesData.forEach((movie)=>{
-
-              if(Number(movieId) === movie.id){
-                  document.querySelector(`.js-movie-${movie.id}`)
-                      .classList.remove('movie-info-off')
-                 
-              }
-          })
-      })
-    });
-}
-  
-function mouseOut(data){
-    document.querySelectorAll('.poster-carousell')
-    .forEach((info) => {
-      info.addEventListener('mouseout', ()=>{
-          const movieId= info.dataset.movieId
-          const moviesData= data.results
-
-          moviesData.forEach((movie)=>{
-
-              if(Number(movieId) === movie.id){
-                  document.querySelector(`.js-movie-${movie.id}`)
-                      .classList.add('movie-info-off')
-                 
-              }
-          })
-      })
-    });
-}
 
